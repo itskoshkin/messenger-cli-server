ec461391c9ff8c96224d19c95dc1650fcc77a0a4
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-27 21:30:23 +0300

    refactoring logging

--- source/Services/AuthService.c
+++ source/Services/AuthService.c
@@ -3,19 +3,21 @@
  */
 
 #include "AuthService.h"
+#include "TimeService.h"
 #include "stdlib.h"
 #include "string.h"
 #include <stdio.h>
+
 #define STR_LEN_MAX 100
 
-bool signIn(char* login, char * password){
+bool signIn(char *login, char *password) {
 
     return false;
 }
 
-char* makeData(char *log, char *pas){
+char *makeData(char *log, char *pas) {
     int size = strlen(log) + strlen(pas) + 2;
-    char * temp = (char*)malloc(size);
+    char *temp = (char *) malloc(size);
     strcat(temp, log);
     strcat(temp, ":");
     strcat(temp, pas);
@@ -24,26 +26,24 @@ char* makeData(char *log, char *pas){
     return temp;
 }
 
-int stringFind(FILE* fr, char* str, long file_size){
+int stringFind(FILE *fr, char *str, long file_size) {
     //go to the begin of file
     fseek(fr, 0, SEEK_SET);
 
     //buffer for checking
-    char* string = (char*)calloc(STR_LEN_MAX, sizeof(char));
+    char *string = (char *) calloc(STR_LEN_MAX, sizeof(char));
     if (!string) exit(EXIT_FAILURE);
-    char* stringbuf = (char*)calloc(STR_LEN_MAX, sizeof(char));
+    char *stringbuf = (char *) calloc(STR_LEN_MAX, sizeof(char));
     if (!stringbuf) exit(EXIT_FAILURE);
 
     //skip the strings while it is not a neccessary key
-    while (strcmp(stringbuf, str))
-    {
+    while (strcmp(stringbuf, str)) {
         //read a string from file
         fgets(string, STR_LEN_MAX, fr);
-        stringbuf = strtok (str, ":");
+        stringbuf = strtok(str, ":");
 
         //if the end of file
-        if (ftell(fr) == file_size)
-        {
+        if (ftell(fr) == file_size) {
             free(string);
             return 0;
         }
@@ -53,30 +53,27 @@ int stringFind(FILE* fr, char* str, long file_size){
     return 1;
 }
 
-bool signUp(char* login, char * password){
+bool signUp(char *login, char *password) {
     //open file
-    FILE* database = fopen("../data/users.txt", "rt");
-    if (!database)
-    {
-        puts("Error open database");
+    FILE *database = fopen("../data/users.txt", "rt");
+    if (!database) {
+        printf("[%s] Error open database\n", getCurrentTime());
         return -2;
     }
     fseek(database, 0, SEEK_END);
     long file_size = ftell(database);
 
     //check database
-    if (stringFind(database, login, file_size))
-    {
-        printf("\nERROR: the login are already used");
+    if (stringFind(database, login, file_size)) {
+        printf("[%s] ERROR: the login are already used\n", getCurrentTime());
         return false;
     }
 
     //add to database
     char *data = makeData(login, password);
     database = fopen("../data/users.txt", "a");
-    if (!database)
-    {
-        puts("Error open database");
+    if (!database) {
+        printf("[%s] Error open database\n", getCurrentTime());
         return -2;
     }
     fprintf(database, "%s", data);
