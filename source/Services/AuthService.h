19c80fba641f53d32575f31d139b945545399b50
ssstoyanov <46551118+ssstoyanov@users.noreply.github.com>
2020-05-27 21:32:35 +0300

    refactoring logging

--- source/Services/AuthService.h
+++ source/Services/AuthService.h
@@ -11,10 +11,6 @@ bool signIn(char *login, char *password);
 
 bool signUp(char *login, char *password);
 
-char *makeData(char *login, char *password);
-
-int stringFind(FILE *database, char *string, long file_size);
-
 #define SERVER_AUTHSERVICE_H
 
 #endif
